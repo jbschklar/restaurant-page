78606a067d83e86da21a2852ad08e9723f17da0b
jbschklar <jbschklar@gmail.com>
2022-08-11 15:39:17 -0400

    added modules and loaders for css and images

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 "use strict";
+import "./style.css";
+import Banner from "./korean-bbq-banner.jpg";
+
 console.log("It's alive!!!");
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,20 @@ module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
 	output: {
-		filename: "main.js",
+		// filename: "main.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
 	},
 };
